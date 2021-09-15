f7badaf829d8636342097d3028cb1a91d3646365
Emma Strickland <emlstrick@gmail.com>
2021-09-15 17:15:42 -0400

    Update readme and more error handling

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # My Guessing Game
 
-TODO
+This original repository was originally forked from Fullstack Academy. I developed UI as well as the JavaScript file based on the test specs provided by Fullstack.
+
+## Usage
+
+1. Clone the repository:
+
+```
+git clone https://github.com/emma-strickland/my-guessing-game
+cd my-guessing-game
+```
+
+2. Install depdencies and start the app:
+   ```
+   npm install
+   ```
--- js/guessing-game.js
+++ js/guessing-game.js
@@ -2,7 +2,7 @@ function generateWinningNumber() {
   return Math.floor(Math.random() * 100) + 1;
 }
 
-function shuffle (arr) {
+function shuffle(arr) {
   let m = arr.length, temp, i;
   // while there are still elements to shuffle
   while (m) {
@@ -33,6 +33,8 @@ class Game {
   }
   playersGuessSubmission(guess) {
     if (isNaN(guess) || guess < 1 || guess > 100) {
+      // TODO: move this into UI
+      document.querySelector('#guess-feedback > h4').innerHTML = 'That is an invalid guess.'
       throw 'That is an invalid guess.'
     } else {
       let num = parseInt(guess)
@@ -46,7 +48,7 @@ class Game {
     if (this.playersGuess === this.winningNumber) {
       this.pastGuesses.push(this.playersGuess)
       feedback = `You Win! The number was ${this.winningNumber} 🥳`;
-      // TODO: move this to a function
+      // TODO: move this into UI
       document.getElementById('hint-button').disabled = true;
       document.getElementById('button-submit').disabled = true;
     }
@@ -57,7 +59,7 @@ class Game {
       this.pastGuesses.push(this.playersGuess);
       if (this.pastGuesses.length >= 5) {
         feedback = `You Lose. The number was ${this.winningNumber} 😢`;
-        // TODO: move this to a function
+        // TODO: move this to into UI
         document.getElementById('hint-button').disabled = true;
         document.getElementById('button-submit').disabled = true;
       }
@@ -115,25 +117,26 @@ const playGame = () => {
 
   // Clear input value when submit is clicked
   const button = document.querySelector('button')
-  button.addEventListener("click", function() {
+
+  button.addEventListener('click', function () {
     const playersGuess = document.querySelector('input').value;
     document.querySelector('input').value = '';
 
     game.playersGuessSubmission(playersGuess)
   });
 
-  // button.addEventListener("keyup", function(event) {
-  //   // Number 13 is the "Enter" key on the keyboard
-  //   if (event.key === 13 || event.key === 'Enter') {
-  //     // Cancel the default action, if needed
-  //     event.preventDefault();
-  //     // Trigger the button element with a click
-  //     document.querySelector("button").click();
-  //   }
-  // });
+  // not working
+  const submit = document.getElementById('button-submit');
+  submit.addEventListener('click', function (event) {
+    const key = event.keyCode || event.key || event.code
+    if (key === 13 || key === 'Enter') {
+      submit.click();
+    }
+  });
+
 
   const hint = document.getElementById('hint-button')
-  hint.addEventListener("click", function() {
+  hint.addEventListener("click", function () {
     let hints = game.provideHint();
     document.getElementById('hint').innerHTML = `The winning number is either ${hints[0]}, ${hints[1]}, or ${hints[2]}`;
     document.getElementById('hint-button').disabled = true;
